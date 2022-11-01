7d10df83eb4e9c3de275eeb1ef8428639abe2921
Cristian Dizeo <dizeoc@gmail.com>
2022-11-01 12:08:46 -0300

    tag price cache

--- views/paginas/listadob.php
+++ views/paginas/listadob.php
@@ -4,6 +4,9 @@ foreach ($consultas as $bicicleta) : ?>
    echo $page === 'index' ? 'col-md-4' : 'col-md-6' ?>">
     <div class="product-item">
     <a href="/bicicleta?id=<?php echo $bicicleta->id; ?>">
+    <?php if($bicicleta->precio > 0){?>
+        <div class="p-2 mb-2 text-white bg-dark font-weight-bold position-absolute" style="top: 0;right: 15px;">$ <?php echo number_format($bicicleta->precio,0,null,'.'); ?></div>
+        <?php } ?>
         <?php foreach ($imageness as $imagen) : ?>
           <?php if ($bicicleta->id == $imagen->bicicletaId){?>
             <img loading="lazy" src="/imagenes/<?php echo trim($imagen->name); ?>" alt="anuncio">
